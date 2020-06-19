1b3244a6a39320c062c200370eb4c74b9e74b0b5
Rogovoy Evgeniy <evg.rogovoy@gmail.com>
2020-06-19 07:56:44 +0300

    Chore: update 1 module

--- module-1/index.js
+++ module-1/index.js
@@ -223,6 +223,23 @@ const var1 = '';
 //     console.log("Неправильный ввод.");
 // }
 
+
+// const userWord = prompt('Введите слово');
+// let phrase;
+
+// if (userWord === 'hello') {
+    // phrase = '1';
+    // console.log();
+    // вызов функция
+// } else {
+    // phrase = '2';
+// } 
+
+// Тернарный оператор
+
+// const result = если условие true ? то выполнить действие : в противном случае;
+// const isTodayAGoodDay = userWord === 'hello' ? phrase === '1' ? console.log("ABC") : '2'
+
 // Switch
 // switch (subscribtion) {
 //     case 'premium': {
@@ -242,486 +259,171 @@ const var1 = '';
 // }
 
 
- // ;jadf;dfjd
-/*
-skdf;sdkfsdfksdk;flskd
-lsjdflsdjflsdjflsdjflj
-dfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf
-sdfsdfsdfsdfsdfsd
-*/
-
-// const number = 4;
-// const string = "Hello World 4";
-// const bool = true
-// const bool = false;
-// const n = null;
-// const u = undefined;
-
-// const name = "Americano";
-// const price = 25;
-// const currency = "UAH";
-// const size = "medium";
-// const isMilk = true;
-
-// const americano = {
-//   name: "Americano",
-//   price: 25,
-//   currency: "UAH",
-//   size: "medium",
-//   isMilk: true
-// };
-
-// const tea = {
-//   name: "Tea",
-//   price: 12,
-//   currency: "UAH",
-//   size: "large",
-//   isMilk: false
-// };
-
-// const nameTea = "Tea";
-// const priceTea = 12;
-// const currencyTea = "UAH";
-// const sizeTea = "large";
-// const isMilkTea = false;
-
-// typeof
-// const n = 3;
-// const s = "Hello";
-// const b = false;
-
-// console.log("Check variable n ", typeof n);
-// console.log("Check variable s", typeof s);
-// console.log("Check variable b", typeof b);
-
-// console.log(tea);
-// console.log(typeof w);
-// console.log(typeof num);
-// console.log(typeof b);
-// console.log(typeof un);
-// console.log(typeof empty);
-// console.log(typeof coffe);
-
-// let answer = prompt('Enter your name');
-// console.log(answer);
-// let answer2 = confirm('Are you happy?');
-// console.log(answer2);
-
-// Create variables
-// console.log(x);
-// // console.log(y);
-// // console.log(z);
-// var x = 5;
-// let y = 10;
-// const z = 50;
-
-// const
-
-// var x = 5;
-// let y = 10;
-// const z = 50;
-// x = 20;
-// console.log(x);
-// y = 30;
-// console.log(y);
-// z = 60;
-// console.log(z);
-
-// duplicate declared
-// var x = 5;
-// var x = 10;
-// console.log(x);
-
-// let y = 20;
-// let y = 30;
-// console.log(y); // Error
-
-// const z = 50;
-// const z = 60;
-// console.log(z); // Error
-
-// if(true) {
-//     var a = 10;
-//     console.log(a);
-// }
-
-// console.log(a);
-
-// Hoisting
-
-// console.log(x); // undefined
-// var x;
-// x = 5;
-// var x = 5;
-
-// console.log(y); // is not defined
-// let y = 10;
-// console.log('Hello');
-
-// console.log(z); // is not defined
-// const z = 50;
-
-// +
-//
-
-// let a = 5;
-// // a = a + 1;
-// // a += 1;
-
-// console.log(a++);
-// console.log(a);
-// console.log(++a);
-
-// let a = 5;
-// let b = 10;
-// let result = a + b;
-// console.log(result);
-// console.log(typeof result);
-
-// let a = 6;
-// let b = false;
-// let c = "12";
-// let d = 1;
-// let result = a + d + c + b;
-// console.log(result);
-// console.log(typeof result);
-
-// let a = "Join ";
-// let b = "us";
-// console.log(a + b);
-// console.log(typeof a + b)
-
-// let a = 3;
-// let b = ' join';
-// console.log(a + b);
-
-// let a = true;
-// let b = " join";
-// console.log(a + b);
-
-// let a = true;
-// let b = 12;
-// console.log(a + b);
-
-// let a = true;
-// let b = true;
-// console.log(a + b);
-
-// let a = true;
-// let b = "false";
-// console.log(a + b);
-
-// let a = 12;
-// let b = null;
-// console.log(a + b);
-
-// let a = undefined;
-// let b = 10;
-// console.log(a + b);
-
-// let a = undefined;
-// let b = false;
-// console.log(a + b);
-// console.log(typeof NaN);
-// NEXT
-// -
-
-// let a = 10;
-// let b = 5;
-// console.log(b - a);
-
-// let a = "join us";
-// let b = "us";
-// let result = a - b;
-// console.log(result);
-// console.log(typeof result);
-
-// let a = 10;
-// let b = "2";
-// let c = 5;
-// console.log(a + b - c);
-
-// let a = 10;
-// let b = "true";
-// console.log(a - b);
-
-// *
-
-// let a = 10;
-// let b = 5;
-// console.log(a * b);
-
-// let a = 2;
-// let b = "start";
-// console.log(a * b);
-
-// let a = 2;
-// let b = "2.2";
-// console.log(a * b);
-
-// let a = 10;
-// let b = undefined;
-// console.log(a * b);
-
-// let a = 10;
-// let b = Infinity;
-// console.log(a / 0);
-
-//  %
-// let a = 10;
-// let b = 3;
-// console.log(a % b);
-
-// let string = "Hello";
-// alert(string);
-// console.log("object");
-
-// let answer = prompt("How old are you");
-// console.log(answer);
-
-// let answer = confirm("Are you ready?");
-// console.log(answer);
-
-// let num1 = prompt("Enter number");
-// // let result = +num1;
-// // console.log(result);
-// let result = Number(num1);
-// let b = 10;
-// console.log(10 + Number(num1));
-// let num2 = prompt('Enter number');
-// let num3 = prompt('Enter number');
-// console.log(Number(num3));
-// console.log(Number(num1) + Number(num2) + Number(num3));
-
-// let a = "20rem2";
-// let b = "150px";
-// let c = "2.5vw";
-// let d = "qweqw25";
-// parsewInt, parseFloat
-
-// let result = parseFloat(b);
-// console.log(result);
-
-// console.log(parseInt(a));
-// console.log(parseInt(b));
-// console.log(parseFloat(c));
-
-// let a = 100.1;
-// let b = 222.2;
-// let result = a + b;
-// let roundResult = result.toFixed(1);
-// let formatedResult = Number(roundResult);
-// console.log(formatedResult);
-// let result = a + b
-// console.log(result.toFixed(2));
-
-// let a = "Hello";
-// console.log(a.length);
-// let question = prompt("Login");
-// console.log(question.length);
-// console.log(question.toUpperCase());
-// console.log(question.toLowerCase());
-// console.log(a[0]);
-// let email = prompt("Enter your email");
-// console.log(email.includes("gmail", "@"));
-// console.log(a.includes('he'));
-// console.log(a.indexOf("l"));
-// console.log(a.indexOf('ll'));
-
-// let name = "Homer";
-// let lastName = "Simpson";
-// let town = "Springfield";
-
-// console.log(name + " " + lastName + " " + "live in " + town);
-// console.log(`${name} ${lastName} live in ${town}`);
-
-// Logic and if
-
-// let age = prompt("Enter your age");
-
-// if (age < 18) {
-//   alert("Grow up");
+// const password = prompt("Enter password");
+// if (password === "admin") {
+//   alert("Welcome admin");
+// } else if (password === "user") {
+//   alert("Welcome user");
+// } else if (password === "batman") {
+//   alert("Welcome batman");
 // } else {
-//   alert("Go to army");
+//   alert("Wrong password");
 // }
 
-// 1) Отримати в користувача інформацію
-// 2) Перевірити з весною
-// 2.1) Якщо співпало то показуємо "Весна"
-// 2.2) Якщо не співпало перевіряємо з Літом
-// 3) Перевірити з літом
-// ...
+// switch (password) {
+//   case "admin":
+//     alert("Welcome admin");
+//     break;
+//   case "user":
+//     alert("Welcome user");
+//     break;
+//   case "batman":
+//     alert("Welcome batman");
+//     break;
+//   default:
+//     alert("Wrong password");
+//     break;
+// }
 
-// let month = Number(prompt("Enter month number"));
-// if (month === 3 || month === 4 || month === 5) {
-//     console.log("Весна");
-// } else if (month > 5 && month < 9) {
-//     console.log("Літо");
-// } else if (month > 9 && month < 12) {
-//     console.log("Осінь");
-// } else if (month === 12 || month === 1 || month === 2) {
-//     console.log("Зима");
+// const isDark = false;
+// let theme;
+
+// if (isDark) {
+//   theme = "dark";
 // } else {
-//     console.log("Введи число від 1 до 12");
+//   theme = "light";
 // }
 
-// let a = 5;
-// let b = "5";
-// console.log(a == b);
-// console.log(a === b);
+// let theme = isDark ? "dark" : "light";
+// let theme = isDark === true ? "dark" : isDark === null ? "light" : "blue";
 
-//
-// let a = NaN,
-//   b = NaN;
-// // console.log(a != b);
-// // console.log(a !== b);
-// console.log(a == b);
-// console.log(a === b);
+// 1) перевірка на роботу
+// 2) лічильник
+// 3) тіло циклу
+// 4) зміна лічильника
 
-// let a = 0,
-//   b = null;
-// console.log(a == b);
-// console.log(a === b);
-//
-// let a = null,
-//   b = null;
-// console.log(a == b);
-// console.log(a === b);
-
-// let a = null,
-//   b = undefined;
-
-// console.log(a == b);
-// console.log(a === b);
-
-// let a = 0,
-// b = undefined;
-// console.log(a == b);
-// console.log(a === b);
-//
-// let a = 'a',
-//     b = null;
-// console.log(a == b);
-// console.log(a === b);
-
-// let a = 'H';
-// let b = 'h';
-
-// console.log(a == b);
-// console.log(a === b);
-
-// let c = "Hello";
-// let d = "Hello";
-// console.log(c == d);
-// console.log(c === d);
-
-// let a = 5;
-// let b = 10;
-
-// console.log(a >= b);
-// let register = true;
-// let isPremium = false;
-
-// console.log(register && isPremium);
-// let login = 'user';
-// let email = 'dou@gmail.com';
-
-// let userData = 'dou@gmail.com';
-// console.log(userData === login || userData === email);
-// || or
-// && and
-// ! not
-
-// console.log('Отримав відпустку' && "Квитки" && 'Train');
-// console.log('Отримав відпустку' || "Квитки" || 'Train');
-
-// false
-// console.log(Boolean(NaN));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
-// console.log(Boolean(-0));
-// console.log(Boolean(0));
-// console.log(Boolean(''));
-// console.log(Boolean(false));
-
-// true
-// console.log(Boolean('add'));
-// console.log(Boolean(Infinity));
-// console.log(Boolean(' '));
-// console.log(Boolean(-5));
-
-// console.log(0 || '' || '0');
-// console.log(0 || null || '0');
-// console.log(0 || undefined || null);
-// console.log(4 + 5 === 8 || 5 * 4 === '20' || 'q' + 'w' === 'w');
-// console.log(0 || '' || 0);
-// console.log(0 || null || 'undefined');
-// console.log(NaN || null || 5);
-// console.log(28 || null || '');
-// console.log(Infinity || 55 || NaN);
-// console.log(undefined || 'true' || NaN);
-
-// console.log('a' && 1 && 0);
-// console.log('a' && 1 && 2);
-// console.log('a' && 0 && 2);
-// console.log('0' && NaN && 2);
-// console.log('' && NaN && 2);
-// console.log(' ' && NaN && 2);
-// console.log({age:25} && 'null' && 2);
-// console.log({age:25} && null && 2);
-// console.log({age:25} && undefined && 2);
-// console.log({age:25} && 8 && -Infinity);
-// console.log(false && 'false' && -Infinity);
-// console.log('true' && -0 && null);
-
-// const correctLogin = 'admin';
-// const userLogin = prompt('Enter login');
-
-// if(userLogin !== correctLogin) {
-//     console.log('Error');
-// } else {
-//     console.log('Hello');
+// let count = 0;
+// while (count < 10) {
+//   console.log(count);
+//   count++;
 // }
 
-// const userNumber = prompt('enter number 1-3');
-// let title = null;
+// let count = 0;
+// do {
+//   console.log(count);
+//   count++;
+// } while (count < 10);
 
-// if (Number(userNumber) === 1) {
-//     title = 'Error';
-// } else if (Number(userNumber) === 2) {
-//     title = 'Congratulation';
-// } else if (Number(userNumber) === 3) {
-//     title = 'Info';
-// } else {
-//     alert ('Wrong number');
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
 // }
 
-// switch (Number(userNumber)) {
-//     case 1:
-//         title = 'Error';
+// for (let i = 1; i <= 10; i++) {
+//   //   if (i % 2 === 1) {
+//   //     continue;
+//   //   }
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// const number = Number.parseInt(prompt('Введите число:'));
+
+// 1 - инициализация счетчика
+
+// 2 - выполняется ли условие
+// 3 - выполнение тела цикла
+// 4 - инкремент
+
+// for (let i = 0; i < 10; i++) {
+    //тело цикла
+    // console.log("index:", i);
+// }
+
+// console.log("Финальный результат:", i);
+
+// while (true) { 
+//     if (i === 1000) {
 //         break;
-//     case 2:
-//         title = 'Congratulation';
-//         break;
-//     case 3:
-//         title = 'Info';
-//         break;
-//     default:
-//         alert('Wrong number');
-//         break;
+//     } else {
+//         i++;
+//     }
 // }
 
-// title = Number(userNumber) === 1 ? 'Error' : Number(userNumber) === 2 ? 'congratulation' : Number(userNumber) === 3 ? 'info' : alert('Wrong');
+// i = i + 1;
+// i += 1;
+// i++;
 
-// console.log(title);
+// i--;
+// i -= 1;
+// i = i - 1;
 
-// const password = prompt('Enter password');
 
-// condition ? true : false
-// let result = null;
-// if(password.length >= 4) {
-//     result = 'correct';
-// } else {
-//     result = 'error';
+let number = 1;
+let numb = 2;
+
+// 2 + 2
+const sm1 = numb++ + ++number;
+
+console.log("NUMB1:", numb);
+console.log("NUMB2:", number);
+
+console.log(sm1);
+
+// if (typeof number !== 'undefined') {
+
+// } else if (typeof number === 'string') {
+
+// } else if (typeof number === 'null') {
+
 // }
-// result = password.length >= 4 || password ? 'correct' : 'error';
+
+// NaN 
+
+
+// switch (typeof number) {
+//     case 'number': {
+//         // выполним 1 действие
+//         break;
+//     }
+//     case 'string': {
+//         // 
+//         break;
+//     }
+//     case 'undefined': {
+//         break;
+//     }
+// }
+
+
+// let isValid = true;
+// let hasError = true;
+
+// do {
+//     const password = prompt('Введите пароль:');
+//     if (password.length === 0) {
+//         console.log("Пароль пустой");
+//         isValid = false;
+//     }
+//     if (password.length < 8) {
+//         console.log("Длина не соответствует.");
+//         isValid = false;
+//     }
+//     if (isValid) {
+//         hasError = false;
+//         break;
+//     }
+// } while (!isValid);
+
+// if (!hasError) {
+
+// }
+
+
+// asdasda
+
+
+
+
+// console.log("Пароль валидный!");
