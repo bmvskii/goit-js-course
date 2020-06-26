cce6729b3afcf70925e84e45e498e1294c766f1b
Rogovoy Evgeniy <evg.rogovoy@gmail.com>
2020-06-26 11:25:57 +0300

    Chore: open 2 module

--- module-2/index.js
+++ module-2/index.js
@@ -102,7 +102,7 @@
 // console.log(array3);
 
 // Методы массивов
-const array = [];
+// const array = [];
 
 // LIFO/FIFO
 
@@ -156,6 +156,192 @@ const array = [];
 // console.log(array2);
 // console.log(array3);
 
-const str = 'GoIT Academy Kharkiv';
+// const str = 'GoIT Academy Kharkiv';
 // str.split(' ')
-console.log(str.split(' ')[0].split('').reverse().join('*').concat(' Academy Kharkiv'));
+// console.log(str.split(' ')[0].split('').reverse().join('*').concat(' Academy Kharkiv'));
+
+// Удаление
+
+// const array = [1, 4, 5, 10, 14];
+// const deletedArray = array.splice(2, 2);
+
+// console.log(deletedArray);
+// console.log(array);
+
+// Вставка
+
+// const resultArray = array.splice(1, 0, "string", "string2");
+
+// console.log(resultArray);
+// console.log(array);
+
+// Замена
+
+// const resultArray1 = array.splice(1, 1, "true", "false");
+// console.log(array);
+
+// array.splice(3);
+// console.log(array);
+
+// Область видимости
+
+// const var1 = 'string';
+
+// console.log(var1);
+
+// var var1 = 'string';
+
+// if (true) {
+//     var var1 = 10;
+//     console.log(var1);
+// }
+
+// for (let i = 0; i < 10; i++) {
+
+// }
+
+// console.log(var1);
+
+
+// hoisting
+// var var1 = undefined;
+
+// console.log(var2);
+// let var2 = 'string';
+
+// if (true) {
+//     var var2 = 10;
+//     console.log(var2);
+// }
+
+// console.log(var2);
+
+// function test(){
+//     let foo = 33;
+
+//     // ...
+
+//     if(foo) {
+//         let foo = (foo + 55); 
+//     }
+// }
+
+
+// test();
+//
+
+// var a = 1;
+// var b = 2;
+
+// if (a === 1) {
+//     var a = 11; // the scope is global
+//     let b = 22; // the scope is inside the if-block
+
+//     console.log(a);  // 11
+//     console.log(b);  // 22
+// }
+
+// console.log(a); // 11
+// console.log(b); // 2
+
+// Syntax error
+
+// let x = 1;
+//
+// {
+//     var x = 2; // SyntaxError for re-declaration
+// }
+
+// const generateRandomNumber = function(from = 0, to = 100) {
+    // return Math.round(Math.random() * (to - from) + from);
+    // console.log("NUMBER IS GENERATED");
+// }
+
+// const hello = function(age, name = 'user') {
+    // console.log("Hello,", name);
+    // return;
+// }
+
+// console.log(hello());
+
+// console.log(generateRandomNumber());
+// console.log(generateRandomNumber(10, 20));
+// console.log(generateRandomNumber(1, 3));
+// console.log(generateRandomNumber(4, 7));
+
+// const getSum = function() {
+//     let total = 0;    
+//     const array = [...arguments];
+//     // const array = Array.from(arguments);
+
+//     for (const arg of array) {
+//         total += arg;
+//     }
+
+//     return total;
+// }
+
+// console.log(getSum(10, 2, 3, 4, 5, 6, 7));
+// console.log(getSum(1, 2, 3, 4));
+
+// const isPasswordValid = function(password) {
+//     if (password.length === 0) {
+//         console.log("Пароль пустой.");
+//         return;
+//     }
+
+//     if (password.length < 8) {
+//         console.log("Недостаточная длина строки.");
+//         return;
+//     }
+
+//     return "Пароль валидный.";
+// }
+
+// 1 условие - сработало 
+// 2 условие - сработало 
+// 3 - пароль валидный
+// console.log(isPasswordValid(""));
+
+// 1 условие - не сработало 
+// 2 условие - сработало 
+// 3 - пароль валидный
+// console.log(isPasswordValid("asdas"));
+
+// console.log(isPasswordValid("asdsadasdasdas"));
+
+// function helloWorld() {
+//     console.log("Hello world");
+// }
+
+const sum1 = function(a, b, c) {
+    return a + b + c;
+} 
+
+const sum2 = (a, b, c) => a + b + c;
+
+const sum3 = function(a, b, c) {
+    if (a === b) {
+        ///
+    }
+
+    return a + b + c;
+} 
+
+const sum4 = (a, b, c) => {
+    if (a === b) {
+        ///
+    }
+
+    return a + b + c;
+};
+
+const sum = (...args) => {
+    let total = 0;
+
+    for (const arg of args) {
+        total += arg;
+    }
+
+    return total;
+};
