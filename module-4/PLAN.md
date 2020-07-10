f71a27a1a3827a132f3f79514552f4b623d7ccd2
Rogovoy Evgeniy <evg.rogovoy@gmail.com>
2020-07-10 13:40:53 +0300

    Feat: update module 4

--- module-4/PLAN.md
+++ module-4/PLAN.md
@@ -19,12 +19,44 @@
 3. Lexical environment (лексическое окружение)
  - Environment record - внутренняя, закрытая от прямого доступа структура движка для хранения в памяти таблицы
  - [[Environment]] - internal property/slot
+ - Создается новое lexical env в функциях, блочных конструкциях и try..catch конструкциях
+ - module/global/functional environment
+ - динамическая и статическая область видимости
 
 4. Closures (Замыкания)
+- the function defined in the closure ‘remembers’ the environment in which it was created.
+- связь между функцией и ее цепочкой областей видимости
+- thanks lexical environment for that 
+- классический пример с счетчиком
+- пример с secret password
+- область видимости - лексическая (статическая)
+
+- Состояние вычисления (code evaluation state)
+- Функция (function)
+- Область (realm)
+- Лексическое окружение
+- Таблица переменных (variable environment)
+
+Доп. чтиво:
+https://medium.com/@sshambir/%D0%BE%D1%81%D0%B2%D0%B0%D0%B8%D0%B2%D0%B0%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-5b83267ef7d1
+
 
 5. Context (this)
-- в глобальной области видимости
-- в методе объекта
-- в функциях обратного вызова (callbacks)
-- в стрелочных функциях
+- понятие контекст
+- значение опеределяется во время вызова функции, а не создания
+- в глобальной области видимости - strict = undefined, non-strict = window
+- в методе объекта - объект
+- в функциях обратного вызова (callbacks) - window
+- в стрелочных функциях - лучшая оптимизация за счет ограничения , привязка к контексту при создании, 
+- стрелки не могут быть перебиндены, первым аргументом должен идти null
 - call, bind, apply
+
+доп. инфо:
+- при вызове new
+- в цепочке прототипов
+- в инлайновом обработчике событий
+- в обработке событий - this первый элемент , на который нажали
+
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/this
+https://www.freecodecamp.org/news/a-guide-to-this-in-javascript-e3b9daef4df1/
+https://flaviocopes.com/javascript-this/
--- module-4/index.js
+++ module-4/index.js
@@ -120,34 +120,208 @@
 // 9. return из foo4
 // 10. продолжает выполнение программы
 
-const x = 10;
+// const x = 10;
 
-const bar = function() {
-  // Local
-    // env: {  }
-    // parent: { Baz }
-  // const x = 5;
-  console.log(x);
-};
+// const bar = function() {
+//   // Local
+//     // env: {  }
+//     // parent: { Baz }
+//   // const x = 5;
+//   console.log(x);
+// };
 
-const baz = function() {
-  // Local
-    // env: { bar }
-    // parent: { Foo }
-  console.log('baz');
-  bar();
-};
+// const baz = function() {
+//   // Local
+//     // env: { bar }
+//     // parent: { Foo }
+//   console.log('baz');
+//   bar();
+// };
 
-const foo = function() {
-  // Local
-    // env: { baz }
-    // parent: { Global }
-  console.log(x);
-  console.log('foo');
-  baz();
-};
+// const foo = function() {
+//   // Local
+//     // env: { baz }
+//     // parent: { Global }
+//   console.log(x);
+//   console.log('foo');
+//   baz();
+// };
 
 // Global
-  // env: { x = 10, foo: function }
-  // parent: { null }
-foo();
+//   env: { x = 10, foo: function }
+//   parent: { null }
+
+// const x = 0;
+
+// function foo() {
+//   Function
+//     env: { x = 1 }
+//     parent: { Global }
+
+//   function foo1() {
+//     Function
+//       parent: { foo }
+
+//     console.log(x);
+//   }
+
+//   foo1();
+
+//   console.log(x);
+// }
+
+// foo();
+
+
+// function createCounter() {
+//   let counter = 0;
+
+//   return function() {
+//     return counter++;
+//   }
+// }
+
+// const counter1 = createCounter();
+// const counter2 = createCounter();
+
+// console.log(counter1());
+// console.log(counter1());
+
+// console.log(counter2());
+
+// const guessPassword = () => {
+//   let secretPassword = 'ADMIN123';
+  
+//   return function() {
+//     return {
+//       check(password) {
+//         return password === secretPassword;
+//       },
+//       updatePassword(newPassword) {
+//         secretPassword = newPassword;
+//       }
+//     }
+//   }
+// }
+
+// const game = guessPassword();
+
+// console.log(game().check("1234"));
+
+// game().updatePassword("password");
+
+// console.log(game().check("password"));
+
+// const createUser = function(name, birthday) {
+//   let hasBirthdayMessageBeenShown = false;
+
+//   return function(message, date) {
+
+//     if (!hasBirthdayMessageBeenShown && date === birthday) {
+//       console.log(`Поздравляем с днем рождения, ${name}`);
+//       hasBirthdayMessageBeenShown = true;
+//     }
+
+//     console.log(`${name}: ${message}`);
+//   }
+// }
+
+// const messageWithUser1 = createUser("Username", "01/01/2020");
+
+// messageWithUser1("Привет, мир!", "01/01/2020");
+// messageWithUser1("Как дела?", "01/01/2020");
+
+// function foo() {
+//   'use strict';
+//   console.log(this);
+// }
+
+// foo();
+// console.log(this);
+
+// const object = {
+//   a: 1,
+//   b: 2,
+//   foo() {
+//     console.log(this);
+//   },
+//   object1: {
+//     a: 3,
+//     b: 4,
+//     foo() {
+//       console.log(this);
+//     }
+//   },
+//   foo1: function() {
+//     console.log(this);
+//   },
+//   foo2: () => {
+//     console.log(this);
+//   }
+// }
+
+// object.foo();
+// object.foo1();
+// object.object1.foo();
+// object.foo2();
+
+// function showName(message, number) {
+//   console.log("NAME:", this.name);
+//   console.log(message);
+//   console.log(number);
+// }
+
+// var name = "Global";
+
+// const fabric1 = {
+//   people: 10,
+//   name: "Fabric1",
+// };
+
+// const fabric2 = {
+//   people: 20,
+//   name: "Fabric2",
+// };
+
+// const fabric3 = {
+//   people: 30,
+//   name: "Fabric3",
+// };
+
+// showName();
+
+// showName.call(fabric3, "Message 3", 10);
+// showName.call(fabric2, "Message 2", 20);
+// showName.call(fabric1, "Message 1", 30);
+
+// showName.apply(fabric3, ["Message 4", 40]);
+// showName.apply(fabric2, ["Message 5", 50]);
+// showName.apply(fabric1, ["Message 6", 60]);
+
+// const showFabric3Name = showName.bind(fabric3);
+
+// showFabric3Name("Message", 100);
+// showFabric3Name("Message1", 200);
+
+const showText = function(showAction) {
+  showAction();
+}
+
+const object = {
+  fullname: "Username",
+  city: "Kharkiv",
+  message: "123",
+  showUserInfo: function() {
+    console.log(`${this.fullname} from ${this.city}`);
+  }
+}
+
+showText(object.showUserInfo.bind(object));
+
+var message = "456";
+const helloWorld = () => {
+  console.log(this.message);
+}
+
+helloWorld.call(null);
+helloWorld.call(object);
